e17b5af26074e3c48b35daeaf0e901bce9e5b662
Gabriel Reis <gabriel.reis@rumosolucoes.com>
2025-12-19 16:28:42 -0300

    .

---
+++ leetCode-codeWars/Desafios/Desafios/CodeWars.cs
@@ -0,0 +1,13 @@
+﻿namespace Desafios;
+
+internal class CodeWars
+{
+
+    public static string PingIt(string str) // Simple Pig Latin
+    {
+        
+
+
+        return "";
+    }
+}
---
+++ leetCode-codeWars/Desafios/Desafios/Program.cs
@@ -0,0 +1,4 @@
+﻿using Desafios;
+
+Console.WriteLine(CodeWars.PingIt("Pig latin is cool")); // igPay atinlay siay oolcay
+Console.WriteLine(CodeWars.PingIt("Hello world !")); // elloHay orldway !
