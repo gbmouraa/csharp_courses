df2f2b5d6aa0b4c95a0940aee7b917f88d5a3f78
Gabriel Reis <gabriel.reis@rumosolucoes.com>
2025-12-29 15:17:45 -0300

    .

---
+++ MeuSiteEmMVC/MeuSiteEmMVC/Models/Botao.cs
@@ -0,0 +1,25 @@
+﻿namespace MeuSiteEmMVC.Models;
+
+public class Botao
+{
+    private int _count = 0;
+
+    public Botao(){}
+    public Botao(int count)
+    {
+        _count = count;
+    }
+
+    public void Add()
+    {
+        _count++;
+    }
+
+    public void Sub()
+    {
+        if (_count > 0) _count--;
+    }
+
+    public int Total => _count;
+
+}
