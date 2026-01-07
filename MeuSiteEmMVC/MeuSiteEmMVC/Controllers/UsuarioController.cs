0fbf9d2c00a1f3dc0f40c0be2ba4f364a12a1e11
gbmouraa <gabrielmoura-cma@hotmail.com>
2026-01-06 23:48:54 -0300

    ✅Adicionado funcionalidade de  adicionar novos usuarios

--- MeuSiteEmMVC/MeuSiteEmMVC/Controllers/UsuarioController.cs
+++ MeuSiteEmMVC/MeuSiteEmMVC/Controllers/UsuarioController.cs
@@ -18,4 +18,31 @@ public class UsuarioController : Controller
         List<UsuarioModel> usuarios = _usuarioRepositorio.BuscarTodos();
         return View(usuarios);
     }
+
+    public IActionResult Criar()
+    { // essa view usa a model nos valores dos campos
+        // porém como aqui não é passado uma model os campos começam vazios
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Criar(UsuarioModel usuario)
+    {
+        try
+        {
+            if (ModelState.IsValid) // caso a model seja válida é adicionada e retorna para Index com msg de sucesso
+            {
+                _usuarioRepositorio.Adicionar(usuario);
+                TempData["MensagemSucesso"] = $"O usuário {usuario.Nome} foi criado com sucesso";
+                return RedirectToAction("Index");
+            }
+
+            return View(usuario); // caso n passe na validação retorna a view Criar com os dados utuais da model
+        }
+        catch (Exception ex)
+        {
+            TempData["MensagemError"] = "Erro ao cadastrar usuário";
+            return RedirectToAction("Index");
+        }
+    }
 }
